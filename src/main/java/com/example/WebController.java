74ab00f586906fb5c4b62ec237928be3b5f81bda
khan-kashtan <zhorshua@gmail.com>
2020-11-25 01:46:09 +0300

    interim state with json result

--- src/main/java/com/example/WebController.java
+++ src/main/java/com/example/WebController.java
@@ -2,11 +2,20 @@ package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 public class WebController {
     @GetMapping("/employees")
-    public String all() {
+    public String shlyapaNaVhode() {
         return "REST CONTrOLLER\n";
+    }
+
+
+    @GetMapping("/employees/{id}")
+    EmployeeNotFoundException one(@PathVariable Long id) {
+// <(``)
+        return new EmployeeNotFoundException(id);
     }
 }
